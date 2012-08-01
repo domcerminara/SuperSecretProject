30a1a210e76df9146bcf7b93f150a2449c2693cf
Dominic Cerminara <d.cerminara@sta.samsung.com>
2012-08-01 11:21:19 -0700

    only house set up; just before trying nifty auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IPL::Application.config.secret_token = '1a812c340cc4f46187a33ff412ff99e62b9e436b99490b860f0b6ae4939ee99bf9319feaad0d481a99b8c92be44b7fd71f200fb6326c504aea2ddcce7759ec90'
